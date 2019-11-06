901133a5e4bc87fef810d3623af60b5014a46ad7
Stephen Benjamin <stephen@redhat.com>
2019-11-06 16:21:09 -0500

    Set capabilitiy for boot

--- pkg/provisioner/ironic/ironic.go
+++ pkg/provisioner/ironic/ironic.go
@@ -807,6 +807,20 @@ func (p *ironicProvisioner) getUpdateOptsForNode(ironicNode *nodes.Node, checksu
 		},
 	)
 
+	// uefi boot
+	if p.host.Spec.BIOS.BootMode == metal3v1alpha1.Uefi {
+		if ironicNode.Properties["capabilities"] != "boot_mode:uefi" {
+			updates = append(
+				updates,
+				nodes.UpdateOperation{
+					Op:    op,
+					Path:  "/properties/capabilities",
+					Value: "boot_mode:uefi",
+				},
+			)
+		}
+	}
+
 	return updates, nil
 }
 
